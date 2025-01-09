708be3dd6a673f04296f90d401ac917779d06393
iklankerjakosongmalaysia <134831327+devOneGithub@users.noreply.github.com>
2025-01-09 21:59:14 +0800

    v1.2.1

--- next.config.ts
+++ next.config.ts
@@ -3,16 +3,16 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* Other config options here */
   reactStrictMode: true,
-  eslint: {
-    ignoreDuringBuilds: true, // Disable ESLint checks during builds
-  },
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
-  },
+  // eslint: {
+  //   ignoreDuringBuilds: true, // Disable ESLint checks during builds
+  // },
+  // typescript: {
+  //   // !! WARN !!
+  //   // Dangerously allow production builds to successfully complete even if
+  //   // your project has type errors.
+  //   // !! WARN !!
+  //   ignoreBuildErrors: true,
+  // },
 };
 
 export default nextConfig;
